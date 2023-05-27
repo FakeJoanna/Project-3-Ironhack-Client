fc53a105f2a2327959a6f13fb1ff5db91453f472
LKTOKB <theodiamanttt@gmail.com>
2023-05-27 12:10:18 +0300

    Making some test

--- src/pages/ProfilePage/ProfilePage.jsx
+++ src/pages/ProfilePage/ProfilePage.jsx
@@ -111,11 +111,14 @@ function ProfilePage() {
             <div className="userInfoDiv">
 
               <div className="userInfoTextDiv">
+
                 <h2>{userInfo.name}</h2>
+
                 {language === "EN" && <p>{userInfo.review.length === 0 ? "No reviews yet" : userInfo.review.length + " reviews"}</p> }
                 {language === "FR" && <p>{userInfo.review.length === 0 ? "Pas de commentaires" : userInfo.review.length + " avis"}</p> }
                 {language === "ES" && <p>{userInfo.review.length === 0 ? "Ninguna reseña por ahora" : userInfo.review.length + " reseñas"}</p> }
                 {userInfo.followers && <p>{userInfo.followers + " followers"}</p>}
+
               </div>
               
               <div className="userButtonsDivWrapper">
